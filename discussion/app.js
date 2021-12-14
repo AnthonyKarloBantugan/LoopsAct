eae28a933efc8be047abd887d095cc14eb87c8b0
AnthonyBantugan <anthonykarlo.bantugan@gmail.com>
2021-12-14 21:19:33 +0800

    done with accessing elements in string with loop

--- discussion/app.js
+++ discussion/app.js
@@ -93,3 +93,45 @@ function countUsingForLoop() {
         }
     }
 }
+// =================================================
+
+// How to access elements of a string using repetition control structures.
+// ====================================
+
+// get the input of the user
+
+function accessElementsInString() {
+    // get the input of the user using DOM selectors
+    let name = document.getElementById('userName').value;
+    // alert(typeof name); //this is just a checker
+    let textLength = document.getElementById('stringLength');
+
+    // validate and make sure that input is NOT equivalent to blank
+    if (name !== '') {
+        // response if true
+
+        // analyze the value that will be inserted by the user
+            // 1.determine the length of the string.
+                // => invoke the "length" property of a string using (.) notation.
+        textLength.innerHTML = 'The string is ' + name.length + ' characters long.';
+
+        // Upon accessing elements inside a string, this can be done so, using [] square brackets.
+        // keep on mind we can access each element through the use of its index number/count
+        // the count will start from 0 (first character inside the string corresponds to the number 0), the next is 1 and up to the "nth" number.
+            // console.log(name[0]);
+            // console.log(name[1]);
+            // console.log(name[2]);
+            // console.log(name[3]);
+            // console.log(name[4]);
+            // console.log(name[5]);
+            // console.log(name[6]);
+        // we will now use the concept of loops in order to produce a much more flexible response for the user's input
+        for (let startIndex = 0; startIndex < name.length; startIndex++) {
+            // access each element and display it inside the console
+            alert(name[startIndex]);    
+        }
+    } else {
+        //response if false
+        alert('value is invalid');
+    }
+}
